1b2836231f2ec47f27cc67868d9ca2408650f0c1
Riteek Rakesh <riteek7.12@gmail.com>
2024-04-09 22:59:51 +0530

    dockerise the backend

--- backendAPI/index.js
+++ backendAPI/index.js
@@ -20,7 +20,7 @@ app.use(
 // test api 
 app.get('/test',(req,res)=>{
     try{
-        res.status(200).json({message: 'API is working'})
+        res.status(200).json({message: 'API is working!'})
     }catch(error){
         res.status(500).json({message: error.message})
     }
@@ -29,7 +29,7 @@ app.get('/test',(req,res)=>{
 // Get all users
 app.get('/users',async (req,res)=>{
     try{
-        const users = await prisma.users.findMany();
+        const users = await prisma.user.findMany();
         res.status(200).json(users)
     }catch(error){
         res.status(400).json({message: error.message})
@@ -37,7 +37,7 @@ app.get('/users',async (req,res)=>{
 });
 
 //Get user by id
-app.get('user/:id', async (req,res) =>{
+app.get('/users/:id', async (req,res) =>{
     try{
         const user = await prisma.user.findUnique({
             where:{
@@ -51,13 +51,27 @@ app.get('user/:id', async (req,res) =>{
 })
 
 //create a user
-app.post('/user', async(req,res)=>{
+app.post('/users', async(req,res)=>{
     try{
         const user = await prisma.user.create({
             data:req.body
         })
         res.sendStatus(201).json(user);
     }catch(error){
+        res.sendStatus(400).json({message:error.message})
+    }
+})
+
+app.delete('/users/:id', async(req,res)=>{
+    try{
+        const user =  await prisma.user.delete({
+            where:{
+                id: Number(req.params.id)
+            } ,
+        });
+
+        res.status(200).json(user);
+    }catch(err){
         res.sendStatus(400).json({message:error.message})
     }
 })
